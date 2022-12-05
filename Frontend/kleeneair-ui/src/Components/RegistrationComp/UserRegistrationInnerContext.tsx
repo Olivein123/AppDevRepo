59a5100933d193b01c5312687c2bd293c91debb8
Keith <kbdiamantes@gmail.com>
2022-12-05 09:36:08 +0800

    UI-registration-display-fix_v9.35am
    Registration UI display fix

--- Frontend/kleeneair-ui/src/Components/RegistrationComp/UserRegistrationInnerContext.tsx
+++ Frontend/kleeneair-ui/src/Components/RegistrationComp/UserRegistrationInnerContext.tsx
@@ -1,6 +1,5 @@
 import { Button, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
 import { useState } from 'react';
-import { Typography } from '@mui/material';
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 
@@ -33,29 +32,57 @@ export default function RegistrationInnerContext() {
 
 
     return (
-        <FormControl className="centerForm">
-            <FormControl sx={{ ml:3, mb: 3,  width: '40ch' }} variant="outlined">
-                <TextField id="username" label="Username" type="text" variant="outlined" />
+        <FormControl className="centerDisplayForm">
+            <div>
+
+            <div className="registerInLine">
+                    <FormControl sx={{ ml:3, mb:3 }} variant="outlined">
+                        <TextField id="username" label="Username" type="text" variant="outlined" />
+                    </FormControl>
+
+                    <FormControl sx={{ ml:3, mb:3 }} variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                        <OutlinedInput id="outlined-adornment-password" type={values.showPassword ? 'text' : 'password'} value={values.password} onChange={handleChange('password')}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end">
+                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
+            </div>
+            
+            <div className="registerInLine">
+            <FormControl sx={{ ml:3, mb: 3}} variant="outlined">
+                <TextField id="firstname" label="First Name" type="text" variant="outlined" />
             </FormControl>
-            <FormControl sx={{ ml: 3, mb: 3, width: '40ch' }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                <OutlinedInput id="outlined-adornment-password" type={values.showPassword ? 'text' : 'password'} value={values.password} onChange={handleChange('password')}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end">
-                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>} label="Password" />
+            <FormControl sx={{ ml: 3, mb: 3}} variant="outlined">
+                <TextField id="middlename" label="Middle Name" type="text" variant="outlined" />
             </FormControl>
-            <FormControl sx={{ ml: 3, mb: 3, width: '40ch' }} variant="outlined">
-                <TextField id="emailaddress" label="Email Address" type="email" variant="outlined" />
+            <FormControl sx={{ ml: 3, mb: 3 }} variant="outlined">
+                <TextField id="lastname" label="Last Name" type="text" variant="outlined" />
             </FormControl>
-            <FormControl sx={{ ml: 3, mb: 3, width: '40ch' }} variant="outlined">
-                <TextField id="contactnumber" label="Contact Number" type="tel" variant="outlined" />
-            </FormControl>
-            <FormControl sx={{ ml: 3, mb: 3, width: '40ch' }} variant="outlined">
+                </div>
+
+            <FormControl sx={{ ml: 3, mb: 3 }} variant="outlined">
                 <TextField id="address" label="Address" type="text" variant="outlined" />
             </FormControl>
+            
+
+            <div className="registerInLine">
+            <FormControl sx={{ ml: 3, mb: 3 }} variant="outlined">
+                <TextField id="contactnumber" label="Contact Number" type="tel" variant="outlined" />
+            </FormControl>
+            <FormControl sx={{ ml: 3, mb: 3}} variant="outlined">
+                <TextField id="licensenumber" label="License Number" type="tel" variant="outlined" />
+            </FormControl>
+            </div>
+            
+
+
             <Grid container>
                 <Grid item>
                     <Button sx={{ ml: 3, mb: 3, marginRight: 0 }} variant="contained" href="/login">Sign Up</Button>
@@ -64,8 +91,24 @@ export default function RegistrationInnerContext() {
                     <p>Already have an account? <a href="/login">Log in</a></p>
                 </Grid>
             </Grid>
+            </div>
         </FormControl>
         
         
         ); 
 }
+
+
+
+/*
+             <FormControl sx={{ ml: 3, mb: 3, width: '65.5ch' }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput id="outlined-adornment-password" type={values.showPassword ? 'text' : 'password'} value={values.password} onChange={handleChange('password')}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end">
+                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>} label="Password" />
+            </FormControl> 
+ */
